f7f9d00d0418e4282e521c790e0d437d09186fb0
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-12 01:44:54 +0200

    Adding more elements to change Theme

--- Emiliaville/JS/ToggleTheme.js
+++ Emiliaville/JS/ToggleTheme.js
@@ -1,4 +1,14 @@
 function ToggleTheme() {
-    let element = document.body;
-    element.classList.toggle("theme");
+    let body = document.body;
+    let div = document.querySelectorAll("body div");
+    let footer = document.querySelector("footer");
+    let p = document.querySelectorAll("p");
+    p.forEach((element) => {
+        element.classList.toggle("divtheme");
+    });
+    div.forEach((element) => {
+        element.classList.toggle("divtheme");
+    });      
+    body.classList.toggle("theme");
+    footer.classList.toggle("theme");
 }
