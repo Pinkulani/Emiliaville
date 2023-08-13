e4598c43a7564aa571b517ff46afd1217ce52ecc
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-13 04:19:54 +0200

    Making this function work without any help 😎

--- Emiliaville/JS/ToggleCSS.js
+++ Emiliaville/JS/ToggleCSS.js
@@ -2,10 +2,12 @@ let Counter = true;
 
 function ToggleCSS () {
     let head = document.querySelector("head");
-        link = document.createElement("link");
-        link.href = "https://pinkulani.com/Stylesheets/Test.css";
-
-        head.appendChild(link);
     
-    //    Counter = !Counter;
+    link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://pinkulani.com/Stylesheets/Test.css";
+    
+    head.appendChild(link);
+    
+    Counter = !Counter;
 }
