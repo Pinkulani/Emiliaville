261bd996b5831a9b03eef0db839fc4e343a80387
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-13 04:14:41 +0200

    Adding function to append CSS

--- Emiliaville/JS/ToggleCSS.js
+++ Emiliaville/JS/ToggleCSS.js
@@ -1,5 +1,11 @@
 let Counter = true;
 
 function ToggleCSS () {
-    Counter = !Counter;
+    let head = document.querySelector("head");
+        link = document.createElement("link");
+        link.href = "https://pinkulani.com/Stylesheets/Test.css";
+
+        head.appendChild(link);
+    
+    //    Counter = !Counter;
 }
