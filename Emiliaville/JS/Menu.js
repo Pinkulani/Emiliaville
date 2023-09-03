39bf0e0be2fb1297c75833ccfca50111a67d4c95
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-09-04 00:53:46 +0200

    Adding Menu DIV

--- Emiliaville/JS/Menu.js
+++ Emiliaville/JS/Menu.js
@@ -1,5 +1,12 @@
-function Menu(Show, Hide) {
+function Menu() {
+    // Navigation
+    const Navigation = document.createElement("div");
+    document.body.appendChild(Navigation)
+    Navigation.setAttribute("id", "Navigation");
+
+    // Copyright
     const Copyright = document.createElement("p");
     Copyright.innerText = "© Pinkulani 2023-2023";
     document.body.appendChild(Copyright);
+    Navigation.append(Copyright);
 }
