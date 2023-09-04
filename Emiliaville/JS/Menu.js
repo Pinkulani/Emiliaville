4f11d62093aef47d5366ca94f17f62e10af1df85
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-09-04 22:41:51 +0200

    Adding more messy but working code

--- Emiliaville/JS/Menu.js
+++ Emiliaville/JS/Menu.js
@@ -4,6 +4,43 @@ function Menu() {
     document.body.appendChild(Navigation)
     Navigation.setAttribute("id", "Navigation");
 
+    // Title
+    const Title = document.createElement("h1");
+    Title.innerText = "Pinkulani";
+    document.body.appendChild(Title);
+    Navigation.append(Title);
+
+    // Links
+    const Home = document.createElement("a");
+    Home.setAttribute("href", "https://www.pinkulani.com");
+    Home.innerHTML = "Home";
+    document.body.appendChild(Home);
+    Navigation.append(Home);
+
+    const Social = document.createElement("a");
+    Social.setAttribute("href", "https://www.pinkulani.com/Social");
+    Social.innerHTML = "Social";
+    document.body.appendChild(Social);
+    Navigation.append(Social);
+
+    const Archive = document.createElement("a");
+    Archive.setAttribute("href", "https://www.pinkulani.com/Archive");
+    Archive.innerHTML = "Archive";
+    document.body.appendChild(Archive);
+    Navigation.append(Archive);
+
+    const About = document.createElement("a");
+    About.setAttribute("href", "https://www.pinkulani.com/About");
+    About.innerHTML = "About";
+    document.body.appendChild(About);
+    Navigation.append(About);
+
+    const Donate = document.createElement("a");
+    Donate.setAttribute("href", "https://ko-fi.com/pinkulani");
+    Donate.innerHTML = "Donate";
+    document.body.appendChild(Donate);
+    Navigation.append(Donate);
+
     // Copyright
     const Copyright = document.createElement("p");
     Copyright.innerText = "© Pinkulani 2023-2023";
