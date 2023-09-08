ebbbcbd55ece7ac81967402706392a0818f39e3b
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-09-08 18:47:04 +0200

    Create load function

---
+++ Emiliaville/JS/Load.js
@@ -0,0 +1,15 @@
+function Load(Page) {
+    switch (Page) {
+        case Main:
+            console.log("Main");
+            break;
+        
+        case Social:
+            console.log("Social");
+            break;
+        
+        default:
+            console.log("Default");
+            break;
+    }
+}
