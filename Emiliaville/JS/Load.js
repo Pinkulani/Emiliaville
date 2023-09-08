730c76261707441ffa02e09038e990c98807b479
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-09-08 21:04:48 +0200

    Adding Test text

--- Emiliaville/JS/Load.js
+++ Emiliaville/JS/Load.js
@@ -2,6 +2,9 @@ function Load(Page) {
     switch (Page) {
         case Main:
             console.log("Main");
+            Test = document.createElement("p");
+            Test.innerHTML = "AAA";
+            document.getElementById("page").appendChild(Test);
             break;
         
         case Social:
