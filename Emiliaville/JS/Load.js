0eaafbfe30f4753caf6c9980f82331005dcae947
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-09-09 02:39:43 +0200

    Adding Loader

--- Emiliaville/JS/Load.js
+++ Emiliaville/JS/Load.js
@@ -1,18 +1,33 @@
+// Pointer for Content
+let URL = "Hewwo";
+
+// Get Content URL from Storage
 function Load(Page) {
     switch (Page) {
-        case Main:
-            console.log("Main");
-            Test = document.createElement("p");
-            Test.innerHTML = "AAA";
-            document.getElementById("page").appendChild(Test);
+        // Navigation Content
+        case "Home":
+            URL = "https://www.pinkulani.com/Content/Home"
             break;
         
-        case Social:
-            console.log("Social");
+        case "Social":
+            URL = "https://www.pinkulani.com/Content/Social"
             break;
         
+        case "Archive":
+            URL = "https://www.pinkulani.com/Content/Archive"
+            break;
+
         default:
-            console.log("Default");
+            console.log("Default / Error");
             break;
     }
+
+    Loader(URL);
+}
+
+// Append Content to Main
+function Loader(URL) {
+    fetch(URL)
+        .then(Response => Response.text())
+        .then(text => document.getElementById("page").innerHTML = text)
 }
